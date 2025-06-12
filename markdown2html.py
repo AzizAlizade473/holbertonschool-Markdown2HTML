d8fef1945736ef52df6a5e7d8e7d521a57bf0ece
AzizAlizade473 <elizadeeziz2006@gmail.com>
2025-06-12 14:50:04 +0400

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,13 +1,9 @@
-#!/usr/bin/python3
-"""
-Script that converts a Markdown file to HTML.
-Usage: ./markdown2html.py input.md output.html
-"""
+
 
 import sys
 import os
 
-# Check if the number of arguments is correct
+
 if len(sys.argv) < 3:
     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     sys.exit(1)
@@ -15,10 +11,10 @@ if len(sys.argv) < 3:
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-# Check if the input file exists
+
 if not os.path.exists(input_file):
     print(f"Missing {input_file}", file=sys.stderr)
     sys.exit(1)
 
-# Success
+
 sys.exit(0)
